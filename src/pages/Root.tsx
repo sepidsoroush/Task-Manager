3e230b19964fdff825db7d833c2464cbf16df12e
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-07-06 10:16:34 +0300

    added AppShell to root layout

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,15 +1,29 @@
 import { Outlet } from "react-router-dom";
-
+import { AppShell, Navbar } from "@mantine/core";
 import Sidebar from "../components/sidebar/Sidebar";
 
 function RootLayout() {
   return (
-    <div style={{ display: "flex", position: "relative" }}>
-      <Sidebar />
+    <AppShell
+      padding="md"
+      navbar={
+        <Navbar width={{ base: 300 }} height={500} p="xs">
+          <Sidebar />
+        </Navbar>
+      }
+      styles={(theme) => ({
+        main: {
+          backgroundColor:
+            theme.colorScheme === "dark"
+              ? theme.colors.dark[8]
+              : theme.colors.gray[0],
+        },
+      })}
+    >
       <main>
         <Outlet />
       </main>
-    </div>
+    </AppShell>
   );
 }
 
