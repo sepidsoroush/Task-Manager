1895175391e6df7a0df591843d71b11bf54fcaf7
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-07-08 16:43:25 +0300

    added task info modal

--- src/components/tasks/TaskItem.tsx
+++ src/components/tasks/TaskItem.tsx
@@ -1,15 +1,29 @@
 import React from "react";
-import { Card, Text, Badge } from "@mantine/core";
+import { Card, Text, Badge, ActionIcon, Modal, Group } from "@mantine/core";
+import { IconDotsVertical } from "@tabler/icons-react";
+import { useDisclosure } from "@mantine/hooks";
 
 const TaskItem: React.FC<{ taskText: string; taskDate: string }> = (props) => {
+  const [opened, { open, close }] = useDisclosure(false);
+
   return (
     <Card w={150} shadow="sm" p="sm" radius="md" withBorder>
-      {props.taskDate && (
-        <Badge color="pink" variant="light" size="xs">
-          {props.taskDate}
-        </Badge>
-      )}
-      <Text color="dimmed" size="sm" my="xs">
+      <Card.Section withBorder inheritPadding py="xs">
+        <Group position="apart">
+          <ActionIcon onClick={open} variant="light" size="xs" radius="sm">
+            <IconDotsVertical size="1rem" stroke={1.5} />
+          </ActionIcon>
+          <Modal opened={opened} onClose={close} title="Task's info">
+            Info
+          </Modal>
+          {props.taskDate && (
+            <Badge color="pink" variant="light" size="xs">
+              {props.taskDate}
+            </Badge>
+          )}
+        </Group>
+      </Card.Section>
+      <Text color="dimmed" size="sm" lineClamp={3} my="xs">
         {props.taskText}
       </Text>
     </Card>
