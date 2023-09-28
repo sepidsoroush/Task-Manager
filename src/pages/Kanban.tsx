ba227f8f4d1e34fb92ba807c99148e0624bda118
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-09-28 20:50:43 +0300

    update Root layout style

--- src/pages/Kanban.tsx
+++ src/pages/Kanban.tsx
@@ -1,9 +1,15 @@
-import { Box } from "@mantine/core";
+import { Box, useColorModeValue } from "@chakra-ui/react";
 import Tasks from "../components/tasks/Tasks";
 
 const Kanban = () => {
   return (
-    <Box>
+    <Box
+      bg={useColorModeValue("white", "gray.900")}
+      w="full"
+      h="full"
+      px={6}
+      pos="fixed"
+    >
       <Tasks />
     </Box>
   );
--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,29 +1,15 @@
+import React from "react";
 import { Outlet } from "react-router-dom";
-import { AppShell, Navbar } from "@mantine/core";
 import Sidebar from "../components/sidebar/Sidebar";
 
 function RootLayout() {
   return (
-    <AppShell
-      padding="md"
-      navbar={
-        <Navbar width={{ base: 300 }} height={500} p="xs">
-          <Sidebar />
-        </Navbar>
-      }
-      styles={(theme) => ({
-        main: {
-          backgroundColor:
-            theme.colorScheme === "dark"
-              ? theme.colors.dark[8]
-              : theme.colors.gray[0],
-        },
-      })}
-    >
+    <React.Fragment>
+      <Sidebar />
       <main>
         <Outlet />
       </main>
-    </AppShell>
+    </React.Fragment>
   );
 }
 
