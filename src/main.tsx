deadba1b9bd50a849f6c63eb263a2ab68fbfb7e0
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-09-28 17:06:50 +0300

    install ChakraUI

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import store from "./store/index.ts";
+import { ChakraProvider } from "@chakra-ui/react";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ChakraProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ChakraProvider>
   </React.StrictMode>
 );
