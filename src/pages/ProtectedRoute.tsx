ca3e33c8d06c3838b13081ff55222fc5b1a00585
sepidsoroush <s.soroush2012@gmail.com>
2025-04-19 17:39:41 +0300

    add loading spinner to protected route

--- src/pages/ProtectedRoute.tsx
+++ src/pages/ProtectedRoute.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
 import { useAuthContext } from "@/context/AuthContext";
+import { LoadingSpinner } from "@/components/ui/loading-spinner";
 
 type Props = {
   children: ReactNode;
@@ -9,7 +10,16 @@ type Props = {
 const ProtectedRoute: React.FC<Props> = ({ children }) => {
   const { user, loading } = useAuthContext();
 
-  if (loading) return <div>Loading...</div>;
+  if (loading)
+    return (
+      <div className="w-full h-screen flex items-center place-content-center">
+        <LoadingSpinner
+          width={96}
+          height={96}
+          className="flex items-center place-content-center"
+        />
+      </div>
+    );
 
   if (!user) return <Navigate to="/signin" replace />;
 
