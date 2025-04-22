db153a57a78d1f44e3b5bdec4bdb0dc108b16f36
sepidsoroush <s.soroush2012@gmail.com>
2025-04-22 12:35:35 +0300

    update app name

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
       <div className="flex h-16 w-full items-center justify-between md:p-4 p-2">
         <div className="flex items-center gap-2">
           <Logo />
-          <div className="text-lg md:text-xl">Task Manager</div>
+          <div className="text-lg md:text-xl">Chore</div>
         </div>
         <div className="flex flex-row justify-start items-end gap-1 md:gap-2">
           <NewTask />
